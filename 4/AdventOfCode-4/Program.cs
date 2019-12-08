b3f621452b16aa47b71fab14b58afc38d197c508
Matt Wiper <mwiper@gmail.com>
2019-12-07 23:35:38 -0400

    Day 4 program

---
+++ 4/AdventOfCode-4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int start = 158126;
+            const int end = 624574;
+
+            List<string> validPasswords = new List<string>(); 
+
+            foreach (int possiblePassword in Enumerable.Range(start, end))
+            {
+                var tempPosPassword = possiblePassword
+                    .ToString()
+                    .ToCharArray();
+
+                if(HasDuplicateChars(tempPosPassword) && NumberNeverDecreases(tempPosPassword))
+                {
+                    validPasswords.Add(new string(tempPosPassword));
+                }
+            }
+        }
+
+        private static bool HasDuplicateChars(char[] testValue) => testValue
+            .Zip(testValue
+                .Skip(1), (n, m) => new { n, m })
+            .Any(x => x.n == x.m);
+
+        private static bool NumberNeverDecreases(char[] testValue)
+        {
+            char prevVal = ' ';
+
+            foreach(var testChar in testValue)
+            {
+                if (prevVal == ' ')
+                {
+                    prevVal = testChar;
+                    continue; 
+                }
+
+                if (prevVal <= testChar)
+                {
+                    prevVal = testChar;
+                    continue; 
+                }
+    
+                return false; 
+            }
+
+            return true;
+        }
+    }
+}
